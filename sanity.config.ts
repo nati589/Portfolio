9ed7887b5bc4af75e3a17bc62c7615677b8b39f3
nati589 <natimok55@gmail.com>
2023-04-27 06:18:31 -0900

    Project schemas

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk"
+// import project from "./sanity/schemas/project-schema";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
     projectId: "34on15oy",
@@ -7,7 +9,8 @@ const config = defineConfig({
     title: "My Personal Website",
     apiVersion: "2023-24-04",
     basePath: "/admin",
-    plugins: [deskTool()]
+    plugins: [deskTool()],
+    schema: {types: schemas}
 })
 
 export default config
